2583f4e6aabf2a5447e690954e0a3596a71a02fc
James Osterhage <josterhage@gmail.com>
2020-12-15 22:29:39 -0600

    Finished Instructions 0xB0 - 0xBF (load immediates into GP registers)

--- CPU/i8088/EU-InstructionSet-0xB0-0xBF.cs
+++ CPU/i8088/EU-InstructionSet-0xB0-0xBF.cs
@@ -32,62 +32,74 @@ namespace CPU.i8088
 
             private void mov_ah_i8()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.AH = tempBL;
             }
 
             private void mov_ch_i8()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.CH = tempBL;
             }
 
             private void mov_dh_i8()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.DH = tempBL;
             }
 
             private void mov_bh_i8()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.BH = tempBL;
             }
 
             private void mov_ax_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.AX = TempB;
             }
 
             private void mov_cx_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.CX = TempB;
             }
 
             private void mov_dx_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.DX = TempB;
             }
 
             private void mov_bx_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.BX = TempB;
             }
 
             private void mov_sp_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.SP = TempB;
             }
 
             private void mov_bp_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.BP = TempB;
             }
 
             private void mov_si_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.SI = TempB;
             }
 
             private void mov_di_i16()
             {
-                throw new NotImplementedException();
+                fetch_next_from_queue();
+                registers.DI = TempB;
             }
         }
     }
