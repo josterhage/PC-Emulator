de9a688234783f27163b1ff32440bad89dfbcad1
James Osterhage <josterhage@gmail.com>
2021-01-21 00:11:21 -0600

    Created PageRegisterChangeEventArgs class

---
+++ CPU/i8237/PageRegisterChangeEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemBoard.i8237
+{
+    public class PageRegisterChangeEventArgs :EventArgs
+    {
+        public byte Page { get; set; }
+        public byte Value { get; set; }
+
+        public PageRegisterChangeEventArgs(byte page, byte value)
+        {
+            Page = page;
+            Value = value;
+        }
+            
+    }
+}
