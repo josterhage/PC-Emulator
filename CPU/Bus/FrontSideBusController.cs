38e76b5b88c3d4ae7d42888222bd87b7eb2bfcbb
James Osterhage <josterhage@gmail.com>
2021-01-21 00:07:25 -0600

    Updated read/write cycle to follow 8088 timings.

--- CPU/Bus/FrontSideBusController.cs
+++ CPU/Bus/FrontSideBusController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SystemBoard.i8088;
+using SystemBoard.i8237;
 using SystemBoard.i8259;
 using SystemBoard.SystemClock;
 
@@ -17,8 +18,8 @@ namespace SystemBoard.Bus
     {
         private readonly MemoryBusController memoryBusController;
         private readonly IOBusController ioBusController;
-        private readonly InterruptController interruptController;
         private readonly MainTimer timer = MainTimer.GetInstance();
+        private readonly DmaController dmaController;
 
         private delegate void Write();
         //private readonly Processor cpu;
@@ -30,41 +31,88 @@ namespace SystemBoard.Bus
             set
             {
                 _address = value;
-                switch (S02)
-                {
-                    case BusState.instructionFetch:
-                    case BusState.readMemory:
-                        Data = memoryBusController.Read(_address);
-                        break;
-                    case BusState.writeMemory:
-                        memoryBusController.Write(_address, Data);
-                        break;
-                    case BusState.readPort:
-                        Data = ioBusController.Read(_address);
-                        break;
-                    case BusState.writePort:
-                        ioBusController.Write(_address, Data);
-                        break;
-                    default:
-                        break;
-                }
+                busCycle = 0;
             }
         }
         public byte Data { get; set; }
         public BusState S02 { get; set; }
         public Segment S34 { get; set; }
         public bool S5 { get; set; }
+        public bool Lock { get; set; }
 
-        public FrontSideBusController(MemoryBusController memoryBusController, IOBusController ioBusController)
+        private bool _hold;
+        public bool Hold
+        {
+            get => _hold;
+            set {
+                if (value)
+                    while (S02 != BusState.passive || Lock) ;
+                _hold = value;
+                if(_hold)
+                    do_hold();
+            }
+        }
+
+        private int busCycle;
+
+        public FrontSideBusController(MemoryBusController memoryBusController, IOBusController ioBusController, DmaController dmaController)
         {
             this.memoryBusController = memoryBusController;
             this.ioBusController = ioBusController;
+            this.dmaController = dmaController;
+
+            dmaController.RegisterFrontSideBusController(this);
+
+            busCycle = -1;
+            timer.TockEvent += OnTockEvent;
         }
 
-        private void wait(int clock_cycles)
+        private void do_hold()
         {
-            var ticks = timer.TotalTicks;
-            while ((timer.TotalTicks - ticks) < (ulong)clock_cycles) ;
+            dmaController.Hlda = true;
+            while (_hold) ;
+            dmaController.Hlda = false;
+        }
+
+        protected void OnTockEvent(object sender, TimerEventArgs e)
+        {
+            if (busCycle < 0)
+                return;
+
+            switch (S02)
+            {
+                case BusState.instructionFetch:
+                case BusState.readMemory:
+                    if (busCycle == 0)
+                        Data = memoryBusController.Read(_address);
+                    else if (busCycle == 2)
+                        Data = 0;
+                    break;
+                case BusState.writeMemory:
+                    if (busCycle == 0)
+                        memoryBusController.Write(_address, Data);
+                    else if (busCycle == 2)
+                        Data = 0;
+                    break;
+                case BusState.readPort:
+                    if (busCycle == 0)
+                        Data = ioBusController.Read(_address);
+                    else if (busCycle == 2)
+                        Data = 0;
+                    break;
+                case BusState.writePort:
+                    if (busCycle == 0)
+                        ioBusController.Write(_address, Data);
+                    else if (busCycle == 2)
+                        Data = 0;
+                    break;
+                default:
+                    break;
+            }
+            if (busCycle < 2)
+                busCycle++;
+            else
+                busCycle = -1;
         }
     }
 }
